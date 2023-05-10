37c534549cee1a10c74eb481638bac1ff85d8287
Phanty <saitek123@op.pl>
2023-05-10 07:46:08 +0200

    Added mnist transformation into graph

--- MNIST_GCN.py
+++ MNIST_GCN.py
@@ -9,22 +9,22 @@ from torchvision import datasets, transforms
 class GCN(torch.nn.Module):
     def __init__(self):
         super(GCN, self).__init__()
-        self.conv1 = GCNConv(1, 128)
-        self.conv2 = GCNConv(128, 128)
-        self.conv3 = GCNConv(128, 256)
-        self.fc1 = torch.nn.Linear(256, 128)
-        self.fc2 = torch.nn.Linear(128, 10)
+        self.conv1 = GCNConv(1, 64)
+        self.conv2 = GCNConv(64, 128)
+
+        self.fc1 = torch.nn.Linear(128, 64)
+        self.fc2 = torch.nn.Linear(64, 10)
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
         x = F.relu(self.conv1(x, edge_index))
         x = F.relu(self.conv2(x, edge_index))
-        x = F.relu(self.conv3(x, edge_index))
+
         x = global_max_pool(x, batch)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        return F.relu(x)
+        return F.log_softmax(x, dim=1)
 
 # Load the MNIST dataset as a graph
 train_dataset = MNISTSuperpixels(root='.', train=True)
